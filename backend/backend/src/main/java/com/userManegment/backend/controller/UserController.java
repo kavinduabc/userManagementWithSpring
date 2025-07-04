f3057bb4048f508e338353757d0f650738e6df73
kavinduabc <piumalranepura2002@gmail.com>
2025-07-04 10:45:13 +0530

    done strat user controller

--- backend/backend/src/main/java/com/userManegment/backend/controller/UserController.java
+++ backend/backend/src/main/java/com/userManegment/backend/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.userManegment.backend.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+// user for create rest controller
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/" )
 public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "Call users";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(){
+
+        return "add user";
+    }
+
 }
