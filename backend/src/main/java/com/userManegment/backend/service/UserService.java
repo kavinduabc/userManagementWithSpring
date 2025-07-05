dd2d63f9dd00f13191d0bd39fcab243cc9e0e1b4
kavinduabc <piumalranepura2002@gmail.com>
2025-07-05 21:29:56 +0530

    done

--- backend/src/main/java/com/userManegment/backend/service/UserService.java
+++ backend/src/main/java/com/userManegment/backend/service/UserService.java
@@ -1,17 +1,41 @@
 package com.userManegment.backend.service;
 
+import com.userManegment.backend.dto.UserDto;
+import com.userManegment.backend.model.User;
 import com.userManegment.backend.repo.USerRepo;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class UserService {
-    //abstract to business logic
+
     @Autowired
     private USerRepo userRepo;
-    //get data for controller through dto
 
+    @Autowired
+    private ModelMapper modelMapper;
 
+    public List<UserDto> getAllUsers() {
+        List<User> userList = userRepo.findAll();
+        return modelMapper.map(userList, new TypeToken<List<UserDto>>() {}.getType());
+    }
+
+   public UserDto saveUser(UserDto userDto){
+        userRepo.save(modelMapper.map(userDto, User.class));
+        return userDto;
+   }
+   public UserDto upadateUser(UserDto userDto){
+        userRepo.save(modelMapper.map(userDto, User.class));
+        return userDto;
+   }
+   public String deleteUser(UserDto userDto){
+        userRepo.delete(modelMapper.map(userDto, User.class));
+        return "user deleted";
+   }
 }
